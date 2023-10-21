c5814113936274c2ccd93e8ce4507412906728b2
ireneannx <irene2104@gmail.com>
2023-10-21 11:54:23 +0200

    implement user preferences

--- frontend/src/pages/Settings/index.tsx
+++ frontend/src/pages/Settings/index.tsx
@@ -1,5 +1,7 @@
 // import { Link } from "react-router-dom";
 import {Accordion, AccordionItem} from "@nextui-org/react";
+import {RadioGroup, Radio} from "@nextui-org/react";
+import {Switch} from "@nextui-org/react";
 
 export const SettingsPage = () => {
   return (
@@ -12,7 +14,31 @@ export const SettingsPage = () => {
           aria-label="Accordion 2"
           title="Filters and Preferences"
         >
-          some more text
+          <Switch defaultSelected>
+          Allow Requests for Cash
+          </Switch>
+          <div className="pt-4" >
+            Accept requests only from: 
+          </div>
+          <div>
+            <Switch className="pt-4" defaultSelected>
+              Women
+            </Switch>
+          </div>
+          <div>
+            <Switch className="pt-4" defaultSelected>
+              My Contacts
+            </Switch>
+          </div>
+          <div>
+          <RadioGroup className="pt-4" label="Users with ratings">
+            <Radio value="1star" className="pt-2">1 star and higher</Radio>
+            <Radio value="2star" className="pt-2">2 star and higher</Radio>
+            <Radio value="3star" className="pt-2">3 star and higher</Radio>
+            <Radio value="4star" className="pt-2">4 star and higher</Radio>
+            <Radio value="5star" className="pt-2">5 star</Radio>
+          </RadioGroup>
+          </div>
         </AccordionItem>
       </Accordion>
     );
